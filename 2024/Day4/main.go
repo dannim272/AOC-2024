52944b80651faeaa9ad456bbf046421d1b543982
Daniil <dmorozkin26@gmail.com>
2025-01-01 22:08:00 +0000

    start of day4

--- 2024/Day4/main.go
+++ 2024/Day4/main.go
@@ -3,29 +3,54 @@ package main
 import (
 	"fmt"
 	"os"
-	"encoding/csv"
+	"bufio"
 )
 
-func extractLine(line []string, line1, line2 string) {
-	for i:=0;i<11;i++ {
-		if i == 0{
-			line1 = line[i]
-		}
-		if i == 1{
-			line2 = line[i]
+func hor(lines string,i,total int) int  {
+	if string(lines[i:i+4]) == "XMAS"{
+		fmt.Println(string(lines[i:i+4]))
+		total++
+	}
+	if string(lines[i:i+4]) == "SAMX"{
+		fmt.Println(string(lines[i:i+4]))
+		total++
+	}
+	return total
+}
+
+func ver(table []string,count,total int) int {
+	xmas := 0
+	for i:=0;i<count;i++ {
+		if string(table[i][9]) == "X" {
+			xmas++
+		} else if string(table[i][9]) == "M" && xmas == 1{
+			xmas++
+		} else if string(table[i][9]) == "A" && xmas == 2{
+			xmas++
+		} else if string(table[i][9]) == "S" && xmas == 3{
+			xmas++
 		}
 	}
-	fmt.Println(line)
+	if xmas == 4 {
+		total++
+	}
+	return total
 }
 
 func main() {
-	var line1 string
-	var line2 string
-	file, err := os.Open("input.csv")
-	if err != nil {
-		fmt.Println(err)
+	total := 0
+	count := 0
+	table := []string{}
+	file, _ := os.Open("prac.csv")
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines := scanner.Text()
+		for i:=0;i<len(lines)-3;i++{
+			total = hor(lines,i,total)
+		}
+		table = append(table,lines) 
+		count++
 	}
-	reader := csv.NewReader(file)
-	line,_ := reader.Read()
-	extractLine(line,line1,line2)
+	total = total + ver(table,count,total)
+	fmt.Println(total)
 }
